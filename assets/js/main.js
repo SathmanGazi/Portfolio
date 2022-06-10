e5eb1e52a9f38b8d9411c1dbf65bd7d0caa28e0f
Musa Al Sathman Gazi <gazi@usf.edu>
2022-06-11 04:37:09 +0600

    Updated files for Experiences,Work,Services

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,22 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
 const navMenu = document.getElementById('nav-menu'),
   navToggle = document.getElementById('nav-toggle'),
-  navClose = doucment.getElementById('nav-close');
+  navClose = document.getElementById('nav-close');
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
 if (navToggle) {
   navToggle.addEventListener('click', () => {
-    navMenu.classList.add('show-menu');
-  });
+    navMenu.classList.add('show-menu')
+  })
 }
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
 if (navClose) {
   navClose.addEventListener('click', () => {
-    navMenu.classList.remove('show-menu');
-  });
+    navMenu.classList.remove('show-menu')
+  })
 }
 
 /*==================== REMOVE MENU MOBILE ====================*/
@@ -27,7 +27,7 @@ function linkAction() {
   // When we click on each nav__link, we remove the show-menu class
   navMenu.classList.remove('show-menu');
 }
-navLink.forEach(n => n.addEventListener('click', linkAction));
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
 const skillsContent = document.getElementsByClassName('skills__content'),
@@ -47,8 +47,26 @@ function toggleSkills() {
 skillsHeader.forEach((el) => {
   el.addEventListener('click', toggleSkills)
 });
-/*==================== QUALIFICATION TABS ====================*/
 
+/*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+  tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>{
+  tab.addEventListener('click', ()=>{
+    const target = document.querySelector(tab.dataset.target)
+
+    tabContents.forEach(tabContent =>{
+      tabContent.classList.remove('qualification__active')
+    })
+    target.classList.add('qualification__active')
+
+    tab.forEach(tab =>{
+      tab.classList.remove('qualification__active')
+    })
+    tab.classList.add('qualification__active')
+  })
+})
 /*==================== SERVICES MODAL ====================*/
 
 /*==================== PORTFOLIO SWIPER  ====================*/
